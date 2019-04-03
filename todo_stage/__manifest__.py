00bb2436a618de3b66ad15f04afa6884d7d70e57
jfkode <josefcoperez@outlook.com>
2019-04-03 23:12:23 +0200

    first commit

---
+++ todo_stage/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Add Stages and Tags to To-Dos',
+    'description': 'Organize To-Do Tasks using Stages and Tags',
+    'author': 'José Francisco Pérez',
+    'depends': ['todo_app'],
+}
